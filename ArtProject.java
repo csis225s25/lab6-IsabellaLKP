c32b30514f005e967e9c459664b91c1bf01cbb7c
IsabellaLKP <in24pafu@siena.edu>
2025-02-26 11:25:59 -0500

    Completed our art project

---
+++ ArtProject.java
@@ -0,0 +1,70 @@
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * Lab 6 starter example
+ * 
+ * @author Jim Teresco
+ * @author Ira Goldstein
+ * @version Spring 2025
+ */
+
+// a class that extends JPanel to override the paintComponent method,
+// allowing us to interact with Java's graphics system
+class GraphicsPanel extends JPanel  {
+
+    @Override
+    public void paintComponent(Graphics g)  {
+
+        // first, we should call the paintComponent method we are
+        // overriding in JPanel
+        super.paintComponent(g);
+
+        // the Graphics object passed to this method has many methods
+        // we can use to draw in the area of the panel, one of which
+        // allows us to draw a String at a given x,y position
+
+
+        
+        g.setColor(Color.BLUE);
+        g.fillOval(getWidth()/2-15, getHeight()/2, 30,30);
+        g.setColor(Color.RED);
+        g.fillRect(getWidth()/2+15, getHeight()/2-10, 30, 20);
+
+        g.setColor(Color.BLACK);
+        g.drawLine(getWidth()/2,250,200,250);
+        int[] x = {getWidth()/2,40,70};
+        int[] y={getHeight()/2,100,150};
+        g.fillPolygon(x, y , 3);
+        g.drawArc(getWidth()/2-10,getHeight()/2-25, 50,50,  90, 180);
+
+    }
+}
+
+public class ArtProject implements Runnable {
+
+    /**
+     * The run method to set up the graphical user interface
+     */
+    @Override
+    public void run() {
+
+        // the usual JFrame setup steps
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame("HelloGraphics");
+        frame.setPreferredSize(new Dimension(500, 500));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // construct JPanel with a custom paintComponent method
+        JPanel panel = new GraphicsPanel();
+        frame.add(panel);
+
+        // display the window we've created
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String args[]) {
+        javax.swing.SwingUtilities.invokeLater(new ArtProject());
+    }
+}
