1bc787e4437da8d4f0aa5bcc68ef8704e4459fce
IsabellaLKP <in24pafu@siena.edu>
2025-03-11 17:18:24 -0400

    Added a couple of comments to the file with names

--- ArtProject.java
+++ ArtProject.java
@@ -2,10 +2,12 @@ import java.awt.*;
 import javax.swing.*;
 
 /**
- * Lab 6 starter example
+ * creating our very own art project 
  * 
  * @author Jim Teresco
  * @author Ira Goldstein
+ * @author Isabella Pafundi
+ * @author Colin Fisher
  * @version Spring 2025
  */
 
@@ -23,19 +25,23 @@ class GraphicsPanel extends JPanel  {
         // the Graphics object passed to this method has many methods
         // we can use to draw in the area of the panel, one of which
         // allows us to draw a String at a given x,y position
-
-
         
+        //Oval
         g.setColor(Color.BLUE);
         g.fillOval(getWidth()/2-15, getHeight()/2, 30,30);
+        //Rectangle
         g.setColor(Color.RED);
         g.fillRect(getWidth()/2+15, getHeight()/2-10, 30, 20);
 
+        //line
         g.setColor(Color.BLACK);
         g.drawLine(getWidth()/2,250,200,250);
+
+        //polygon
         int[] x = {getWidth()/2,40,70};
         int[] y={getHeight()/2,100,150};
         g.fillPolygon(x, y , 3);
+        //arc
         g.drawArc(getWidth()/2-10,getHeight()/2-25, 50,50,  90, 180);
 
     }
